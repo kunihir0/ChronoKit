2d498547566afcea204f7fbc1b30dabc6de3329a
kunihir0 <kunihiro@tutanota.com>
2025-09-10 07:02:42 -0600

    feat: Hide debug elements and restructure settings
    - Hide the developer section in release builds using the DEBUG flag.
    - Create a new 'Bypass' section for the SSL pinning toggle.
    - Move the Media Vault to the 'Downloads' section and add an icon.

--- Makefile
+++ Makefile
@@ -11,4 +11,9 @@ TWEAK_NAME = ChronoKit
 ChronoKit_FILES = $(shell find Sources/ChronoKit -name '*.swift') $(shell find deps/SQLite.swift/Sources/SQLite -name '*.swift') $(shell find Sources/ChronoKitC -name '*.m' -o -name '*.c' -o -name '*.mm' -o -name '*.cpp' -o -name '*.x')
 ChronoKit_CFLAGS = -fobjc-arc -ISources/ChronoKitC/include
 
+ifeq ($(DEBUG), 1)
+	ChronoKit_CFLAGS += -DDEBUG=1
+	ChronoKit_SWIFTFLAGS += -DDEBUG
+endif
+
 include $(THEOS_MAKE_PATH)/tweak.mk
--- Sources/ChronoKit/UI/SettingsViewController.swift
+++ Sources/ChronoKit/UI/SettingsViewController.swift
@@ -30,7 +30,11 @@ public class SettingsViewController: UIViewController, UITableViewDataSource, UI
     }
 
     public func numberOfSections(in tableView: UITableView) -> Int {
+        #if DEBUG
+        return 6
+        #else
         return 5
+        #endif
     }
 
 
@@ -41,10 +45,20 @@ public class SettingsViewController: UIViewController, UITableViewDataSource, UI
         if section == 1 { // Feed section
             return 2
         }
-        if section == 3 { // Developer section
-            return 6
+        if section == 2 { // Downloads section
+            return 2
         }
-        if section == 4 { // About section
+        if section == 3 { // Bypass section
+            return 1
+        }
+        if section == 4 { // Developer or About section
+            #if DEBUG
+            return 4
+            #else
+            return 3
+            #endif
+        }
+        if section == 5 { // About section (only in DEBUG)
             return 3
         }
         return 1
@@ -60,8 +74,14 @@ public class SettingsViewController: UIViewController, UITableViewDataSource, UI
         case 2:
             return "Downloads"
         case 3:
-            return "Developer"
+            return "Bypass"
         case 4:
+            #if DEBUG
+            return "Developer"
+            #else
+            return "About"
+            #endif
+        case 5:
             return "About"
         default:
             return nil
@@ -109,29 +129,30 @@ public class SettingsViewController: UIViewController, UITableViewDataSource, UI
                 cell.accessoryView = switchView
             }
         case 2:
-            cell.textLabel?.text = "High Quality"
-            cell.detailTextLabel?.text = "Download videos in high quality"
-
-            let switchView = UISwitch(frame: .zero)
-            switchView.setOn(UserDefaults.standard.bool(forKey: "high_quality"), animated: true)
-            switchView.addTarget(self, action: #selector(highQualityToggled(_:)), for: .valueChanged)
-            cell.accessoryView = switchView
-        case 3:
             if indexPath.row == 0 {
-                cell.textLabel?.text = "SSL Bypass"
-                cell.detailTextLabel?.text = "Enable SSL Pinning Bypass"
+                cell.textLabel?.text = "High Quality"
+                cell.detailTextLabel?.text = "Download videos in high quality"
 
                 let switchView = UISwitch(frame: .zero)
-                switchView.setOn(UserDefaults.standard.bool(forKey: "ssl_bypass_enabled"), animated: true)
-                switchView.addTarget(self, action: #selector(sslBypassToggled(_:)), for: .valueChanged)
+                switchView.setOn(UserDefaults.standard.bool(forKey: "high_quality"), animated: true)
+                switchView.addTarget(self, action: #selector(highQualityToggled(_:)), for: .valueChanged)
                 cell.accessoryView = switchView
             } else if indexPath.row == 1 {
                 cell.textLabel?.text = "Media Vault"
+                cell.imageView?.image = UIImage(systemName: "folder.fill")
                 cell.accessoryType = .disclosureIndicator
-            } else if indexPath.row == 2 {
-                cell.textLabel?.text = "Media Vault"
-                cell.accessoryType = .disclosureIndicator
-            } else if indexPath.row == 3 {
+            }
+        case 3:
+            cell.textLabel?.text = "Enable SSL Pinning"
+            cell.detailTextLabel?.text = "Enable SSL Pinning Bypass"
+
+            let switchView = UISwitch(frame: .zero)
+            switchView.setOn(UserDefaults.standard.bool(forKey: "ssl_bypass_enabled"), animated: true)
+            switchView.addTarget(self, action: #selector(sslBypassToggled(_:)), for: .valueChanged)
+            cell.accessoryView = switchView
+        case 4:
+            #if DEBUG
+            if indexPath.row == 0 {
                 cell.textLabel?.text = "Debug Download All URLs"
                 cell.detailTextLabel?.text = "Download all URLs for a photo album for debugging"
 
@@ -139,7 +160,7 @@ public class SettingsViewController: UIViewController, UITableViewDataSource, UI
                 switchView.setOn(UserDefaults.standard.bool(forKey: "debug_download_all_urls"), animated: true)
                 switchView.addTarget(self, action: #selector(debugDownloadAllURLsToggled(_:)), for: .valueChanged)
                 cell.accessoryView = switchView
-            } else if indexPath.row == 4 {
+            } else if indexPath.row == 1 {
                 cell.textLabel?.text = "Log All Headers"
                 cell.detailTextLabel?.text = "Log all HTTP headers for debugging"
 
@@ -147,11 +168,39 @@ public class SettingsViewController: UIViewController, UITableViewDataSource, UI
                 switchView.setOn(UserDefaults.standard.bool(forKey: "log_all_headers"), animated: true)
                 switchView.addTarget(self, action: #selector(logAllHeadersToggled(_:)), for: .valueChanged)
                 cell.accessoryView = switchView
-            } else if indexPath.row == 5 {
+            } else if indexPath.row == 2 {
                 cell.textLabel?.text = "View Debug Log"
                 cell.accessoryType = .disclosureIndicator
             }
-        case 4:
+            #else
+            // This is the About section in non-debug builds
+            if indexPath.row == 0 {
+                cell.textLabel?.text = "Project Repo"
+                cell.accessoryType = .disclosureIndicator
+            } else if indexPath.row == 1 {
+                cell.textLabel?.text = "License"
+                cell.detailTextLabel?.text = "Apache License 2.0"
+                cell.selectionStyle = .default
+            } else if indexPath.row == 2 {
+                cell.textLabel?.text = "Credits"
+                let button = UIButton(type: .system)
+                button.setTitle("View Credits", for: .normal)
+                
+                let creditsMenu = UIMenu(title: "Credits", children: [
+                    UIAction(title: "DouX", handler: { _ in self.openURL(string: "https://github.com/kunihir0/DouX") }),
+                    UIAction(title: "tiktokusregion", handler: { _ in self.openURL(string: "https://github.com/iGerman00/tiktokusregion.git") }),
+                    UIAction(title: "tiktok-god", handler: { _ in self.openURL(string: "https://github.com/haoict/tiktok-god.git") }),
+                    UIAction(title: "TikTok-Tweaks", handler: { _ in self.openURL(string: "https://github.com/tuxi/TikTok-Tweaks.git") })
+                ])
+                
+                button.menu = creditsMenu
+                button.showsMenuAsPrimaryAction = true
+                button.sizeToFit()
+                cell.accessoryView = button
+            }
+            #endif
+        case 5:
+            // This is the About section in debug builds
             if indexPath.row == 0 {
                 cell.textLabel?.text = "Project Repo"
                 cell.accessoryType = .disclosureIndicator
@@ -185,13 +234,14 @@ public class SettingsViewController: UIViewController, UITableViewDataSource, UI
 
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        if indexPath.section == 3 {
+        if indexPath.section == 2 {
             if indexPath.row == 1 {
                 let vaultVC = VaultCreatorsViewController()
                 self.navigationController?.pushViewController(vaultVC, animated: true)
-            } else if indexPath.row == 2 {
-                debugLastImage()
-            } else if indexPath.row == 5 {
+            }
+        } else if indexPath.section == 4 {
+            #if DEBUG
+            if indexPath.row == 2 {
                 let logContent = FileLogger.shared.readLog()
                 let logVC = UIViewController()
                 let textView = UITextView(frame: logVC.view.bounds)
@@ -200,7 +250,16 @@ public class SettingsViewController: UIViewController, UITableViewDataSource, UI
                 logVC.view.addSubview(textView)
                 self.navigationController?.pushViewController(logVC, animated: true)
             }
-        } else if indexPath.section == 4 {
+            #else
+            // This is the About section in non-debug builds
+            if indexPath.row == 0 {
+                openURL(string: "https://github.com/kunihir0/ChronoKit")
+            } else if indexPath.row == 1 {
+                openURL(string: "https://www.apache.org/licenses/LICENSE-2.0")
+            }
+            #endif
+        } else if indexPath.section == 5 {
+            // This is the About section in debug builds
             if indexPath.row == 0 {
                 openURL(string: "https://github.com/kunihir0/ChronoKit")
             } else if indexPath.row == 1 {
